b40dfb2155685a17cbdf247975b984ca246d5125
muchammadwr <muchammadwr@gmail.com>
2026-09-05 22:27:32 +0700

    preprocessing categorical dan numerical

--- dashboard/preprocessing.py
+++ dashboard/preprocessing.py
@@ -7,18 +7,19 @@ import pandas as pd
 # ================================================
 
 # Encoder
-encoder_AgeGroup = joblib.load("models/encoder_AgeGroup.joblib")
+encoder_TransactionType = joblib.load("models/encoder_TransactionType.joblib")
+encoder_Location = joblib.load("models/encoder_Location.joblib")
 encoder_Channel = joblib.load("models/encoder_Channel.joblib")
 encoder_CustomerOccupation = joblib.load("models/encoder_CustomerOccupation.joblib")
-encoder_Location = joblib.load("models/encoder_Location.joblib")
-encoder_TransactionType = joblib.load("models/encoder_TransactionType.joblib")
+encoder_AgeGroup = joblib.load("models/encoder_AgeGroup.joblib")
 
 # Scaler
 scaler_TransactionAmount = joblib.load("models/scaler_TransactionAmount.joblib")
-scaler_AccountBalance = joblib.load("models/scaler_AccountBalance.joblib")
 scaler_CustomerAge = joblib.load("models/scaler_CustomerAge.joblib")
+scaler_AccountBalance = joblib.load("models/scaler_AccountBalance.joblib")
 scaler_TransactionDuration = joblib.load("models/scaler_TransactionDuration.joblib")
 scaler_LoginAttempts = joblib.load("models/scaler_LoginAttempts.joblib")
+scaler_AccountBalance = joblib.load("models/scaler_AccountBalance.joblib")
 
 
 # ================================================
@@ -34,3 +35,30 @@ def data_preprocessing(data):
         Pandas DataFrame: Dataframe that contain all the preprocessed data
     """
     data = data.copy()
+    df = pd.DataFrame()
+    # Encoder
+    df["TransactionType"] = encoder_TransactionType.transform(
+        data["TransactionType"][0]
+    )
+    df["Location"] = encoder_Location.transform(data["Location"][0])
+    df["Channel"] = encoder_Channel.transform(data["Channel"])
+    df["CustomerOccupation"] = encoder_CustomerOccupation(
+        data["encoder_CustomerOccupation"][0]
+    )
+    df["AgeGroup"] = encoder_AgeGroup.transform(data["AgeGroup"])
+
+    # Scaling
+    df["TransactionAmount"] = scaler_TransactionAmount.transform(
+        data["scaler_TransactionAmount"]
+    )
+    df["CustomerAge"] = scaler_CustomerAge.transform(data["scaler_CustomerAge"])
+    df["AccountBalance"] = scaler_AccountBalance.transform(
+        data["scaler_AccountBalance"]
+    )
+    df["TransactionDuration"] = scaler_TransactionDuration.transform(
+        data["scaler_TransactionDuration"]
+    )
+    df["LoginAttempts"] = scaler_LoginAttempts.transform(data["scaler_LoginAttempts"])
+    df["AccountBalance"] = scaler_AccountBalance.transform(
+        data["scaler_AccountBalance"]
+    )
